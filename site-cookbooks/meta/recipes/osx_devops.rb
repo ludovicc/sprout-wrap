439e6712345df02386326fea92932320962d135d
Ludovic Claude <ludovic.claude54@googlemail.com>
2013-05-11 00:41:01 +0200

    New meta packages

---
+++ site-cookbooks/meta/recipes/osx_devops.rb
@@ -0,0 +1,7 @@
+include_recipe "sprout-osx-apps::csshx"
+include_recipe "sprout-osx-apps::ssh_copy_id"
+include_recipe "sprout-osx-apps::tunnelblick"
+include_recipe "sprout-osx-apps::vagrant"
+include_recipe "sprout-osx-apps::virtualbox"
+#todo: nimrod
+#todo: jmxtrans
---
+++ site-cookbooks/meta/recipes/web_development.rb
@@ -0,0 +1,11 @@
+include_recipe "sprout-osx-apps::chrome"
+include_recipe "sprout-osx-apps::chrome_canary"
+include_recipe "sprout-osx-apps::firefox"
+include_recipe "sprout-osx-apps::firefox_aurora"
+include_recipe "sprout-osx-apps::freeruler"
+include_recipe "sprout-osx-apps::jpegoptim"
+include_recipe "sprout-osx-apps::optipng"
+include_recipe "sprout-osx-apps::node_js"
+include_recipe "sprout-osx-apps::phantomjs"
+include_recipe "sprout-osx-apps::selenium_webdriver"
+
