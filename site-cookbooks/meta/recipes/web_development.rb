c589c99fc35c94fe79ee7cb375c5bd1d645b3ec8
Ludovic Claude <ludovic.claude54@googlemail.com>
2013-05-25 21:45:42 +0200

    Ignore Firefox aurora, broken

--- site-cookbooks/meta/recipes/web_development.rb
+++ site-cookbooks/meta/recipes/web_development.rb
@@ -1,7 +1,7 @@
 include_recipe "sprout-osx-apps::chrome"
 include_recipe "sprout-osx-apps::chrome_canary"
 include_recipe "sprout-osx-apps::firefox"
-include_recipe "sprout-osx-apps::firefox_aurora"
+#include_recipe "sprout-osx-apps::firefox_aurora" -- broken for now
 include_recipe "sprout-osx-apps::freeruler"
 include_recipe "sprout-osx-apps::jpegoptim"
 include_recipe "sprout-osx-apps::optipng"
